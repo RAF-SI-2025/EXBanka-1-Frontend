1231a78c4f6af692ed78f8293c864025233696e9
marija <marijasimic1422@gmail.com>
2026-03-11 19:18:06 +0100

    chore: configure Jest, Prettier, ESLint, Husky + lint-staged + directory scaffold

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+        useESM: false,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+    '\\.(svg|png|jpg)$': '<rootDir>/src/__tests__/utils/fileMock.ts',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/utils/setupTests.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+}
+
+export default config
