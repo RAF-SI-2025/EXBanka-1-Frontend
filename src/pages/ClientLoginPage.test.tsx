d5d900bbad27d853b03f28afe9c83d8f7b939f61
marija <marijasimic1422@gmail.com>
2026-03-21 17:46:43 +0100

    fix: remove unused render import from ClientLoginPage.test.tsx

--- src/pages/ClientLoginPage.test.tsx
+++ src/pages/ClientLoginPage.test.tsx
@@ -1,6 +1,5 @@
-import { screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { render } from '@testing-library/react'
 import { MemoryRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
