388cfbb25a7a10316b4a65362db4e8d0e6533c2c
Vasilije Lupsic <vasilije.lupsic45@gmail.com>
2026-04-07 00:29:49 +0200

    revert axios to localhost

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://bytenity.com'
+//const API_BASE_URL = 'http://bytenity.com'
+const API_BASE_URL = 'http://localhost:8080'
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
