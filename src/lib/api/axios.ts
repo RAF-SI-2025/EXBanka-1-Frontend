f643b275e61a1341021589fd0e930570a9621f67
lukasavic <lukavicsa12@gmail.com>
2026-04-10 16:26:24 +0200

    CI/CD

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://bytenity.com'
+const API_BASE_URL = 'https://bytenity.com'
 // const API_BASE_URL = 'http://localhost:8080'
 
 export const apiClient = axios.create({
